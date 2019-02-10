1ec3c1a61a1c8811ed8a14d48c8de06f2d0f5102
Andrew Erlanger <andrew.erlanger@gmail.com>
2019-02-09 19:01:34 -0500

    adding calfw (calendar for org)

--- init.el
+++ init.el
@@ -135,6 +135,7 @@
      (setq calendar-latitude 41.2)
      (setq calendar-longitude -73.7)
      (setq calendar-location-name "Armonk, NY"))
+    ((calfw calfw-org))
     ((cc-mode)
      (add-hook 'c-mode-hook
                (lambda ()
@@ -471,7 +472,7 @@ _q_ quit
     ("53f65fd5042438d3f0d484dda7cae17e1105ec87de1ac52fee6941247f49098a" "34b9a79a465da3e91300b99ba3b2e533cbfc777ae39887be3c5e93469d183614" "655b7e2225ced08b3a31f086a5357eca10f2bedd40c481e532e570f4d3db62bb" "90e339f5fbc3f739c6bdb82f3553c0fe8377b5b44586414d6ec64951af1fd99b" "ae8da78bab18b45212184256d397ab713383374cc717b6f8229da235f9a55ce9" "2b16f250227e1bb0e98d14dd2327ca4fdbf3b54752b898033d4499f96121e4cb" "5f9bccf201fd171f630ba426d9870069534363bd92e327419bf269dd0345a0e6" "b25d2fffca5d821cff86895eb03d8e72af37419ec612474692db26aff34e2abf" "de8448a27a8f9cf378e31f3023d329cda6743ee2cb9c68a8db87f5d728c72ee1" "d2636cff590df97c4cb5bb6ad7f1f3f72ac841acedb6e5259a29573862637364" default)))
  '(package-selected-packages
    (quote
-    (dired-subtree magit markdown-mode hydra sr-speedbar cider ace-window geiser fill-column-indicator dictionary with-editor page-break-lines paredit rainbow-delimiters smart-tabs-mode undo-tree diminish slime alert sly helm-descbinds helm-describe-modes aggressive-indent ecb haskell-mode flycheck-pyflakes py-autopep8)))
+    (calfw calfw-org dired-subtree magit markdown-mode hydra sr-speedbar cider ace-window geiser fill-column-indicator dictionary with-editor page-break-lines paredit rainbow-delimiters smart-tabs-mode undo-tree diminish slime alert sly helm-descbinds helm-describe-modes aggressive-indent ecb haskell-mode flycheck-pyflakes py-autopep8)))
  '(send-mail-function (quote sendmail-send-it))
  '(znc-servers
    (quote
